5a7b3e8d55e038b6864e7c343739a2aa17aaa6d5
Damar Kuncoro <damarkuncoro@gmail.com>
2026-06-11 11:28:40 +0700

    refactor: standardize event naming convention to identity.user.{created,updated} and improve action parsing logic

--- apps/accounts/config/initializers/satu_raya_identity.rb
+++ apps/accounts/config/initializers/satu_raya_identity.rb
@@ -4,20 +4,21 @@ Rails.application.config.to_prepare do
   # 1. Configure user_sync_publisher to delegate to EventBus (decoupling package from specific host classes)
   SatuRayaIdentity.user_sync_publisher = Object.new.tap do |o|
     def o.execute(action:, user:)
-      event_name = action == "created" ? "identity.user_created" : "identity.user_updated"
+      action_str = action.to_s.downcase
+      event_name = (action_str == "create" || action_str == "created") ? "identity.user.created" : "identity.user.updated"
       SatuRayaCommons::EventBus.publish(event_name, user_id: user.id, sync_action: action)
     end
   end
 
   # 2. Subscribe EventBus events to run the actual user sync use case
-  SatuRayaCommons::EventBus.subscribe("identity.user_created") do |payload, meta|
+  SatuRayaCommons::EventBus.subscribe("identity.user.created") do |payload, meta|
     user = ::Identity::User.find_by(id: payload[:user_id])
     if user
       UseCases::PublishUserSyncEvent.new.execute(action: "created", user: user)
     end
   end
 
-  SatuRayaCommons::EventBus.subscribe("identity.user_updated") do |payload, meta|
+  SatuRayaCommons::EventBus.subscribe("identity.user.updated") do |payload, meta|
     user = ::Identity::User.find_by(id: payload[:user_id])
     if user
       action = payload[:sync_action] || "updated"
